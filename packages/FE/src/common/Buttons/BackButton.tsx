94afb1fa40022ba17c7b74a55da8b78ec85c9056
byeong <qudcks4885@gmail.com>
2024-11-05 14:45:25 +0900

    fix: path alias 적용, 버튼 크기 수정

--- packages/FE/src/common/Buttons/BackButton.tsx
+++ packages/FE/src/common/Buttons/BackButton.tsx
@@ -1,12 +1,12 @@
-import LeftArrow from '../assets/icons/left-arrow.svg?react';
+import LeftArrow from '@/assets/icons/left-arrow.svg?react';
 
 export default function BackButton() {
   return (
     <button
-      className="w-14 h-14 border rounded-full border-none bg-weak flex items-center justify-center"
+      className="w-10 h-10 border rounded-full border-none bg-weak flex items-center justify-center"
       onClick={() => history.back()}
     >
-      <LeftArrow />
+      <LeftArrow className="w-6 h-6" />
     </button>
   );
 }
