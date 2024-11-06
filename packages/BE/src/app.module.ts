67d6dee38e5859d6fac516a2ac2bae47a7a6ba35
glaxyt <glaxyt@naver.com>
2024-11-06 20:38:07 +0900

    feat: MySQL, Redis config 파일 생성

--- packages/BE/src/app.module.ts
+++ packages/BE/src/app.module.ts
@@ -1,28 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { databaseConfig } from './config/database.config';
+import { RedisModule } from '@nestjs-modules/ioredis';
+import { redisConfig } from './config/redis.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
+    // TypeORM 설정
+    TypeOrmModule.forRoot(databaseConfig),
+    
+    // Redis 설정
+    RedisModule.forRoot({
+      type: 'single',  // Redis 연결 타입 지정
+      options: {       // 실제 연결 설정
+        host: redisConfig.host,
+        port: redisConfig.port,
+      }
     }),
-    // TypeOrmModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   useFactory: (configService: ConfigService) => ({
-    //     type: 'mysql',
-    //     host: configService.get('DB_HOST'),
-    //     port: configService.get('DB_PORT'),
-    //     username: configService.get('DB_USERNAME'),
-    //     password: configService.get('DB_PASSWORD'),
-    //     database: configService.get('DB_DATABASE'),
-    //     entities: ['dist/**/*.entity{.ts,.js}'],
-    //     synchronize: configService.get('NODE_ENV') !== 'production',
-    //   }),
-    //   inject: [ConfigService],
-    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
