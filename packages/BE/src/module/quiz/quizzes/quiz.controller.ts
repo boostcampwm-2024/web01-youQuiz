6d54a80357bb38698d50b2a5bcdb55d5791f6124
glaxyt <glaxyt@naver.com>
2024-11-09 19:45:23 +0900

    feat: quiz Controller, Service 구현

--- packages/BE/src/module/quiz/quizzes/quiz.controller.ts
+++ packages/BE/src/module/quiz/quizzes/quiz.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post, UsePipes, ValidationPipe } from "@nestjs/common";
 import { QuizService } from "./quiz.service";
+import { CreateClassRequestDto } from "./dto/create-class.request.dto";
 
 @Controller('api/quiz')
 export class QuizController {
     constructor( private readonly quizService : QuizService) {}
+
+    // class 생성
+    @Post('create-class')
+    @UsePipes(ValidationPipe)
+    async createQuiz(@Body() createClassRequestDto : CreateClassRequestDto) {
+        try {
+            return this.quizService.createQuiz();
+        } catch(err) {
+            console.log(err);
+        }
+    }
 }
--- packages/BE/src/module/quiz/quizzes/quiz.service.ts
+++ packages/BE/src/module/quiz/quizzes/quiz.service.ts
@@ -5,4 +5,8 @@ export class QuizService {
     constructor() {
         console.log('QuizService');
     }
+
+    createQuiz() {
+
+    }
 }
