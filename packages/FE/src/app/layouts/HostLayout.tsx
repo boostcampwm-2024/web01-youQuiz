7bf662a27b843ce974250597f3abd49c8920bd0d
dooohun <ehgns0305@naver.com>
2024-11-11 16:53:27 +0900

    feat: 호스트 레이아웃에 사이드 바 추가
    - quiz-create 페이지에서 사용하는 Sidebar는 삭제

--- packages/FE/src/app/layouts/HostLayout.tsx
+++ packages/FE/src/app/layouts/HostLayout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 import Header from '@/shared/ui/header/Header';
 import SideNav from '@/shared/ui/side-nav/SideNav';
+import SideBar from '@/shared/ui/side-bar/SideBar';
 
 export default function HostLayout() {
   return (
@@ -9,6 +10,7 @@ export default function HostLayout() {
       <Header classTitle="클래스 이름" />
       <div className="flex">
         <SideNav />
+        <SideBar title="클래스 이름" />
         <Outlet />
       </div>
     </div>
