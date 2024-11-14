24ac5eadce5d73cb06bc0bcfd8ebe34eebc9f0d4
glaxyt <glaxyt@naver.com>
2024-11-14 10:00:13 +0900

    fix: 잘못 추가된 속성 id를 update-class.request.dto로 부터 수정

--- packages/BE/src/module/quiz/quizzes/dto/update-class.request.dto.ts
+++ packages/BE/src/module/quiz/quizzes/dto/update-class.request.dto.ts
@@ -1,10 +1,6 @@
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class UpdateClassRequestDto {
-  @IsNumber()
-  @IsNotEmpty()
-  id: number;
-
   @IsString()
   @IsNotEmpty()
   title: string;
