d70b94eee0055a01593cd757ef8d4baf3ffa7218
dooohun <ehgns0305@naver.com>
2024-11-14 17:46:09 +0900

    feat: QuizList 라우트 추가

--- packages/client/src/app/routes/Router.tsx
+++ packages/client/src/app/routes/Router.tsx
@@ -11,12 +11,14 @@ import Nickname from '@/pages/nickname';
 import QuizQuestion from '@/pages/quiz-question';
 import QnA from '@/pages/qna';
 import GuestQnA from '@/pages/guest-qna';
+import QuizList from '@/pages/quiz-list';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route element={<HostLayout />}>
+        <Route path="/quiz-list" element={<QuizList />} />
         <Route path="/quiz/create" element={<QuizCreatePage />} />
         <Route path="/questions" element={<QnA />} />
       </Route>
