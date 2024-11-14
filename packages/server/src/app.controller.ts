3dc439c514c22563509bc02d6cec03e1860fea80
nowChae <nowchae23@gmail.com>
2024-11-14 22:38:22 +0900

    fix: redis 연결 세팅 오류 수정

--- packages/server/src/app.controller.ts
+++ packages/server/src/app.controller.ts
@@ -1,13 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RedisService } from './config/database/redis/redis.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly redisService: RedisService,
+  ) {}
 
   @Get()
   getHello(): string {
     console.log('getHello');
+    this.redisService.set('hi', 'hello');
     return this.appService.getHello();
   }
 }
--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { redisConfig } from './config/database/redis/redis.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -9,9 +8,8 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { UserModule } from './module/user/user.module';
 import { QuizModule } from './module/quiz/quiz.module';
 import { GameGateway } from './module/game/game.gateway';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { RedisService } from './config/database/redis/redis.service';
-
+import { RedisModule } from '@nestjs-modules/ioredis'; // 추가
 @Module({
   imports: [
     UserModule,
@@ -32,17 +30,10 @@ import { RedisService } from './config/database/redis/redis.service';
         autoLoadEntities: configService.get<boolean>('mysql.autoLoadEntities'),
       }),
     }),
-    ClientsModule.register([
-      {
-        name: 'REDIS_SERVICE',
-        transport: Transport.REDIS,
-        options: {
-          host: redisConfig.host,
-          port: redisConfig.port,
-          password: redisConfig.password,
-        },
-      },
-    ]),
+    RedisModule.forRoot({
+      type: 'single',
+      url: process.env.REDIS_URL,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, GameGateway, RedisService],
