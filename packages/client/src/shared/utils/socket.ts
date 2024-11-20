7c3c02cb966400f6d1757551f3cbaee823371b58
dooohun <ehgns0305@naver.com>
2024-11-20 18:08:59 +0900

    feat: socket에서 auth에 sid 추가

--- packages/client/src/shared/utils/socket.ts
+++ packages/client/src/shared/utils/socket.ts
@@ -1,10 +1,13 @@
 import { io, Socket } from 'socket.io-client';
+import { getCookie } from './cookie';
 
 let socket: Socket | null = null;
 
 export function getQuizSocket(): Socket {
   if (!socket) {
-    socket = io(`${import.meta.env.VITE_SERVER_URL}/game`);
+    socket = io(`${import.meta.env.VITE_SERVER_URL}/game`, {
+      auth: { sid: getCookie('sid') },
+    });
   }
   return socket;
 }
