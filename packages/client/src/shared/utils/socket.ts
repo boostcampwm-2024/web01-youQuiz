89685ac8520d322ce3eebc16a5f8b2d8b4fd0bb2
dooohun <ehgns0305@naver.com>
2024-11-21 12:34:28 +0900

    feat: getSocket 함수 transports에 websocket 추가

--- packages/client/src/shared/utils/socket.ts
+++ packages/client/src/shared/utils/socket.ts
@@ -1,10 +1,14 @@
 import { io, Socket } from 'socket.io-client';
+import { getCookie } from './cookie';
 
 let socket: Socket | null = null;
 
 export function getQuizSocket(): Socket {
   if (!socket) {
-    socket = io(`${import.meta.env.VITE_SERVER_URL}/game`);
+    socket = io(`${import.meta.env.VITE_SERVER_URL}/game`, {
+      auth: { sid: getCookie('sid') },
+      transports: ['websocket'],
+    });
   }
   return socket;
 }
