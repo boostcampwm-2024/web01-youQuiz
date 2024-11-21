d315f525a9f0a1769a2cab4e711f104a367150f5
dooohun <ehgns0305@naver.com>
2024-11-21 12:49:00 +0900

    fix: lint 에러 수정
    - 사용하지 않는 error 변수의 에러 메시지를 토스트에 추가
    - base_url의 논리 연산자 수정

--- packages/client/src/pages/quiz-wait/index.tsx
+++ packages/client/src/pages/quiz-wait/index.tsx
@@ -50,7 +50,10 @@ export default function QuizWait() {
       navigator.clipboard.writeText(guestLink);
       toast.success('링크가 복사되었습니다.');
     } catch (error) {
-      toast.error('링크 복사에 실패했습니다.');
+      if (error instanceof Error) {
+        const errorMessage = error.message;
+        toast.error(errorMessage);
+      }
     }
   };
 
