fe52c15de57ad4e8f934dd8e9322f0143aa0d9a7
glaxyt <glaxyt@naver.com>
2024-11-23 19:18:28 +0900

    feat: 실시간 소켓 서버 요청 데이터 인터페이스 barrel 파일 작성

---
+++ packages/shared/interfaces/request/index.ts
@@ -0,0 +1,5 @@
+export * from './master-entry.request.interface';
+export * from './nickname.request.interface';
+export * from './participant-entry.request.interface';
+export * from './show-quiz.request.interface';
+export * from './start-quiz.request.interface';
