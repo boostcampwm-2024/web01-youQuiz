66331a757cb312dd9cf37dc6facfc0060c7eb725
dooohun <ehgns0305@naver.com>
2024-11-24 21:49:39 +0900

    fix: 퀴즈 아이디 수정

--- packages/client/src/pages/quiz-master-session/index.tsx
+++ packages/client/src/pages/quiz-master-session/index.tsx
@@ -37,7 +37,7 @@ const initialTick = {
 };
 
 export default function QuizMasterSession() {
-  const { pinCode, id } = useParams();
+  const { pinCode } = useParams();
   const socket = getQuizSocket();
   const [participantStatistics, setParticipantStatistics] = useState<MasterStatistics>(
     initialParticipantStatistics,
@@ -80,7 +80,7 @@ export default function QuizMasterSession() {
           <div>
             <h1 className="text-xl font-bold mb-2">실시간 통계</h1>
             <p className="text-2xl font-bold mb-2">
-              Q{id}. {quizData.content}
+              Q{quizData.id}. {quizData.content}
             </p>
           </div>
           <div>
