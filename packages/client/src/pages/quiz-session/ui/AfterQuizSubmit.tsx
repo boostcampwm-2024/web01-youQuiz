398b3dbe9cbaac655cead06951a65d41c85709d8
byeong <qudcks4885@gmail.com>
2024-11-24 21:47:11 +0900

    feat: 퀴즈 제출 후 결과 UI 구현

--- packages/client/src/pages/quiz-session/ui/AfterQuizSubmit.tsx
+++ packages/client/src/pages/quiz-session/ui/AfterQuizSubmit.tsx
@@ -8,7 +8,13 @@ interface AfterQuizSubmitProps {
 export default function AfterQuizSubmit({ participantStatistics }: AfterQuizSubmitProps) {
   return (
     <div className="absolute top-0 z-10 p-6 w-full h-full mb-8 backdrop-blur-sm animate-[floatDown_1s_ease-in-out_forwards]">
-      <StatisticsGroup participantStatistics={participantStatistics} />
+      <div className="max-w-4xl mx-auto bg-white rounded-2xl shadow-lg p-6">
+        <StatisticsGroup participantStatistics={participantStatistics} />
+        <div className="flex items-center justify-center">
+          <span className="text-xl font-semibold">나는 몇 등이지?</span>
+          <span></span>
+        </div>
+      </div>
     </div>
   );
 }
