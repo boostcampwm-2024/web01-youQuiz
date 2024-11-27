33ffd31b185366f1ac51d26457773c8962e22b88
byeong <qudcks4885@gmail.com>
2024-11-27 17:53:45 +0900

    refactor: 그리드 아이템 컴포넌트로 분리

--- packages/client/src/pages/quiz-wait/ui/UserGridView.tsx
+++ packages/client/src/pages/quiz-wait/ui/UserGridView.tsx
@@ -1,20 +1,12 @@
-import { useState } from 'react';
-
-interface Guest {
-  nickname: string;
-  character: number;
-  message: string;
-  position: number;
-}
+import { Guest } from '../index';
+import UserGridItem from './UserGridItem';
 
 interface UserGridViewProps {
   guests: Guest[];
 }
 
-const characterNames = ['강아지', '고양이', '돼지', '토끼', '펭귄', '햄스터'];
-
 export default function UserGridView({ guests }: UserGridViewProps) {
-  const [message, setMessage] = useState('');
+  console.log(guests);
 
   return (
     <div className="w-full bg-blue-50 rounded-xl shadow-md">
@@ -29,28 +21,8 @@ export default function UserGridView({ guests }: UserGridViewProps) {
             );
           }
           const participant = guests[index];
-          return (
-            <div className="w-full flex flex-col items-center">
-              <div className="relative w-20 h-20 aspect-square bg-white rounded-full flex items-center justify-center shadow-sm hover:shadow transition-shadow">
-                <img
-                  src={`/src/shared/assets/characters/${characterNames[participant.character]}.png`}
-                  alt={`${characterNames[participant.character]}character`}
-                  className="w-20 h-20 rounded-full"
-                />
-                {participant.message && (
-                  <input
-                    className="absolute -top-12 left-1/2 transform -translate-x-1/2 bg-white p-2 rounded-lg shadow-sm text-sm whitespace-nowrap"
-                    onChange={(e) => setMessage(e.target.value)}
-                    value={message}
-                  />
-                )}
-              </div>
-              <div className="flex justify-center items-center gap-2 w-full mt-2">
-                <div className="w-[10px] h-[10px] rounded-full bg-green-500 animate-blink" />
-                <div className="text-sm text-center truncate">{participant.nickname}</div>
-              </div>
-            </div>
-          );
+
+          return <UserGridItem participant={participant} isMine={participant.isMine} />;
         })}
       </div>
     </div>
