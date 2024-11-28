8bcdfe76d35f5b7f5bc3b8f8781e6308e6c79410
glaxyt <glaxyt@naver.com>
2024-11-28 14:08:16 +0900

    feat: 인터셉터 적용

--- packages/server/src/module/quiz/quizzes/quiz.controller.ts
+++ packages/server/src/module/quiz/quizzes/quiz.controller.ts
@@ -6,56 +6,69 @@ import {
   Param,
   Patch,
   Post,
+  UseInterceptors,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { QuizService } from './quiz.service';
-import { CreateClassRequestDto } from './dto/create-class.request.dto';
-import { CreateQuizListRequestDto } from './dto/create-quizlist.request.dto';
-import { UpdateClassRequestDto } from './dto/update-class.request.dto';
-import { UpdateQuizListRequestDto } from './dto/update-quizlist.request.dto';
+import { CreateClassRequestDto } from './dto/request/create-class.request.dto';
+import { CreateClassResponseDto } from './dto/response/create-class.response.dto';
+import { CreateQuizListRequestDto } from './dto/request/create-quizlist.request.dto';
+import { UpdateClassRequestDto } from './dto/request/update-class.request.dto';
+import { UpdateQuizListRequestDto } from './dto/request/update-quizlist.request.dto';
+import { TransformInterceptor } from '../../interceptors/transform.interceptor';
+import { GetClassResponseDto } from './dto/response/get-class.response.dto';
+// import { ClassResponseDto } from './dto/response/class.response.dto';
 
 @Controller('api')
 export class QuizController {
   constructor(private readonly quizService: QuizService) {}
 
   @Get('classes')
+  @UseInterceptors(new TransformInterceptor(GetClassResponseDto))
   async getClasses() {
-    return await this.quizService.getClasses();
+    const result = await this.quizService.getAllClasses();
+    console.log('result:', result);
+    return result;
   }
 
   @Get('classes/:classId/quizzes')
+  @UseInterceptors(new TransformInterceptor(CreateClassResponseDto))
   async getQuizzes(@Param('classId') classId: number) {
     return await this.quizService.getQuizzesByClassId(classId);
   }
 
   @Post('classes')
+  @UseInterceptors(new TransformInterceptor(CreateClassResponseDto))
   @UsePipes(ValidationPipe)
   async createClass(@Body() dto: CreateClassRequestDto) {
-    return await this.quizService.createClass(dto);
+    const result = await this.quizService.createClass(dto);
+    console.log('result:', result);
+    return result;
   }
 
-  @Delete('classes/:classId')
-  @UsePipes(ValidationPipe)
-  async deleteClass(@Param('classId') classId: number) {
-    return await this.quizService.deleteClass(classId);
-  }
+  // @Delete('classes/:classId')
+  // @UsePipes(ValidationPipe)
+  // @UseInterceptors(new TransformInterceptor(CreateClassResponseDto))
+  // async deleteClass(@Param('classId') classId: number) {
+  //   return await this.quizService.deleteClass(classId);
+  // }
 
-  @Post('classes/:classId/quizzes')
-  @UsePipes(ValidationPipe)
-  async createQuiz(@Param('classId') classId: number, @Body() dto: CreateQuizListRequestDto) {
-    return await this.quizService.createQuiz(classId, dto);
-  }
+  // @Post('classes/:classId/quizzes')
+  // @UsePipes(ValidationPipe)
+  // async createQuiz(@Param('classId') classId: number, @Body() dto: CreateQuizListRequestDto) {
+  //   return await this.quizService.createQuiz(classId, dto);
+  // }
 
-  @Patch('classes/:classId')
-  @UsePipes(ValidationPipe)
-  async updateClass(@Param('classId') classId: number, @Body() dto: UpdateClassRequestDto) {
-    return await this.quizService.updateClass(classId, dto);
-  }
+  // @Patch('classes/:classId')
+  // @UsePipes(ValidationPipe)
+  // async updateClass(@Param('classId') classId: number, @Body() dto: UpdateClassRequestDto) {
+  //   return await this.quizService.updateClass(classId, dto);
+  // }
 
-  @Patch('classes/:classId/quizzes')
-  @UsePipes(ValidationPipe)
-  async updateQuiz(@Param('classId') classId: number, @Body() dto: UpdateQuizListRequestDto) {
-    return await this.quizService.updateQuiz(classId, dto);
-  }
+  // @Patch('classes/:classId/quizzes')
+  // @UsePipes(ValidationPipe)
+  // async updateQuiz(@Param('classId') classId: number, @Body() dto: UpdateQuizListRequestDto) {
+  //   return await this.quizService.updateQuiz(classId, dto);
+  // }
 }
--- packages/server/src/module/quiz/quizzes/quiz.service.ts
+++ packages/server/src/module/quiz/quizzes/quiz.service.ts
@@ -3,16 +3,17 @@ import { DataSource } from 'typeorm';
 import { QuizRepository } from './repositories/quiz.repository';
 import { ChoiceRepository } from './repositories/choice.repository';
 import { ClassRepository } from './repositories/class.repository';
-import { CreateClassRequestDto } from './dto/create-class.request.dto';
-import { CreateQuizListRequestDto } from './dto/create-quizlist.request.dto';
+import { CreateClassRequestDto } from './dto/request/create-class.request.dto';
+import { CreateClassResponseDto } from './dto/response/create-class.response.dto';
+import { CreateQuizListRequestDto } from './dto/request/create-quizlist.request.dto';
 import { ResponseDto } from '../../utils/dto/response.dto';
 import { Quiz } from './entities/quiz.entity';
-import { ClassResponseDto } from './dto/class.response.dto';
-import { QuizResponseDto } from './dto/quiz.response.dto';
-import { UpdateClassRequestDto } from './dto/update-class.request.dto';
-import { UpdateQuizRequestDto } from './dto/update-quiz.request.dto';
-import { UpdateQuizListRequestDto } from './dto/update-quizlist.request.dto';
-import { CreateClassResponseDto } from './dto/create-class.response.dto';
+import { QuizResponseDto } from './dto/response/quiz.response.dto';
+import { UpdateClassRequestDto } from './dto/request/update-class.request.dto';
+import { UpdateQuizRequestDto } from './dto/request/update-quiz.request.dto';
+import { UpdateQuizListRequestDto } from './dto/request/update-quizlist.request.dto';
+import { GetClassResponseDto } from './dto/response/get-class.response.dto';
+import { Class } from './entities/class.entity';
 
 @Injectable()
 export class QuizService {
@@ -23,9 +24,14 @@ export class QuizService {
     private readonly dataSource: DataSource,
   ) {}
 
-  async getClasses(): Promise<ClassResponseDto[]> {
-    const classes = await this.classRepository.findAll();
-    return classes.map(ClassResponseDto.fromEntity);
+  async getAllClasses(): Promise<GetClassResponseDto[]> {
+    const classEntities = await this.classRepository.findAll();
+
+    if (!classEntities || classEntities.length === 0) {
+      throw new NotFoundException(`No classes found`);
+    }
+
+    return classEntities.map((classEntity: Class) => GetClassResponseDto.fromEntity(classEntity));
   }
 
   async getQuizzesByClassId(classId: number): Promise<any> {
@@ -41,125 +47,130 @@ export class QuizService {
   async createClass(createClassRequestDto: CreateClassRequestDto): Promise<CreateClassResponseDto> {
     try {
       const classEntity = await this.classRepository.create(createClassRequestDto);
-      return CreateClassResponseDto.fromEntity(classEntity);
+      console.log('classEntity:', classEntity); // 저장된 엔티티 확인
+
+      const responseDto = CreateClassResponseDto.fromEntity(classEntity);
+      console.log('responseDto:', responseDto); // 변환된 DTO 확인
+
+      return responseDto;
     } catch (error) {
       console.error('error:', error);
       throw error;
     }
   }
 
-  // dto가 여러개라서 처리하기 좀 그러네 quiz, choice는 dto가 아니라 인터페이스로 구현하는게 좋지않을까라는 생각...?
-  async createQuiz(classId: number, quizData: CreateQuizListRequestDto): Promise<ResponseDto> {
-    // 그럼 이 컨트롤러에서 dto 구분이 힘들다
-    // 너무 이 메서드에 책임이 많은게 아닌가 라는 생각도 든다.
-    // const queryRunner = this.dataSource.createQueryRunner();
-    // await queryRunner.connect();
-    // await queryRunner.startTransaction();
-    try {
-      // class_id가 유효한지 확인
-      const is_valid_class = await this.classRepository.findClassById(classId);
-      if (!is_valid_class) {
-        throw new Error(`Class with ID ${classId} not found`);
-      }
+  // // dto가 여러개라서 처리하기 좀 그러네 quiz, choice는 dto가 아니라 인터페이스로 구현하는게 좋지않을까라는 생각...?
+  // async createQuiz(classId: number, quizData: CreateQuizListRequestDto): Promise<ResponseDto> {
+  //   // 그럼 이 컨트롤러에서 dto 구분이 힘들다
+  //   // 너무 이 메서드에 책임이 많은게 아닌가 라는 생각도 든다.
+  //   // const queryRunner = this.dataSource.createQueryRunner();
+  //   // await queryRunner.connect();
+  //   // await queryRunner.startTransaction();
+  //   try {
+  //     // class_id가 유효한지 확인
+  //     const is_valid_class = await this.classRepository.findClassById(classId);
+  //     if (!is_valid_class) {
+  //       throw new Error(`Class with ID ${classId} not found`);
+  //     }
 
-      await Promise.all(
-        quizData.quizzes.map(async (quiz) => {
-          const quizEntity = await this.quizRepository.create(classId, quiz);
-          quiz.choices.map(async (choice) => {
-            this.choiceRepository.create(quizEntity.id, choice);
-          });
-        }),
-      );
+  //     await Promise.all(
+  //       quizData.quizzes.map(async (quiz) => {
+  //         const quizEntity = await this.quizRepository.create(classId, quiz);
+  //         quiz.choices.map(async (choice) => {
+  //           this.choiceRepository.create(quizEntity.id, choice);
+  //         });
+  //       }),
+  //     );
 
-      // await queryRunner.commitTransaction();
+  //     // await queryRunner.commitTransaction();
 
-      return {
-        statusCode: HttpStatus.OK,
-        message: 'Quiz created successfully',
-      };
-    } catch (error) {
-      // await queryRunner.rollbackTransaction();
-      throw new HttpException(
-        {
-          statusCode: HttpStatus.FORBIDDEN,
-          error: `${error}`,
-        },
-        HttpStatus.FORBIDDEN,
-        {
-          cause: error,
-        },
-      );
-    }
-    // } finally {
-    //     await queryRunner.release();
-    // }
-  }
+  //     return {
+  //       statusCode: HttpStatus.OK,
+  //       message: 'Quiz created successfully',
+  //     };
+  //   } catch (error) {
+  //     // await queryRunner.rollbackTransaction();
+  //     throw new HttpException(
+  //       {
+  //         statusCode: HttpStatus.FORBIDDEN,
+  //         error: `${error}`,
+  //       },
+  //       HttpStatus.FORBIDDEN,
+  //       {
+  //         cause: error,
+  //       },
+  //     );
+  //   }
+  //   // } finally {
+  //   //     await queryRunner.release();
+  //   // }
+  // }
 
-  async findAll(): Promise<Quiz[]> {
-    return this.quizRepository.findAll();
-  }
+  // async findAll(): Promise<Quiz[]> {
+  //   return this.quizRepository.findAll();
+  // }
 
-  // id에 해당하는 클래스와 퀴즈, 선택지를 삭제한다.
-  async deleteClass(id: number): Promise<ResponseDto> {
-    try {
-      const classEntity = await this.classRepository.findClassWithRelations(id);
+  // // id에 해당하는 클래스와 퀴즈, 선택지를 삭제한다.
+  // async deleteClass(id: number): Promise<ResponseDto> {
+  //   try {
+  //     const classEntity = await this.classRepository.findClassWithRelations(id);
 
-      if (!classEntity) {
-        throw new HttpException(`Class with ID ${id} not found`, HttpStatus.NOT_FOUND);
-      }
+  //     if (!classEntity) {
+  //       throw new HttpException(`Class with ID ${id} not found`, HttpStatus.NOT_FOUND);
+  //     }
 
-      await this.classRepository.deleteWithRelations(classEntity);
+  //     await this.classRepository.deleteWithRelations(classEntity);
 
-      return {
-        statusCode: HttpStatus.OK,
-        message: 'Class and all related entities deleted successfully',
-      };
-    } catch (error) {
-      throw new HttpException(
-        {
-          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-          error: `Failed to delete class: ${error}`,
-        },
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
-  }
+  //     return {
+  //       statusCode: HttpStatus.OK,
+  //       message: 'Class and all related entities deleted successfully',
+  //     };
+  //   } catch (error) {
+  //     throw new HttpException(
+  //       {
+  //         statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+  //         error: `Failed to delete class: ${error}`,
+  //       },
+  //       HttpStatus.INTERNAL_SERVER_ERROR,
+  //     );
+  //   }
+  // }
 
-  // id에 해당하는 클래스의 정보를 수정한다.
-  async updateClass(id: number, updateData: UpdateClassRequestDto): Promise<ResponseDto> {
-    try {
-      const classEntity = await this.classRepository.findById(id);
+  // // id에 해당하는 클래스의 정보를 수정한다.
+  // async updateClass(id: number, updateData: UpdateClassRequestDto): Promise<ResponseDto> {
+  //   try {
+  //     const classEntity = await this.classRepository.findById(id);
 
-      if (!classEntity) {
-        throw new HttpException(`Class with ID ${id} not found`, HttpStatus.NOT_FOUND);
-      }
+  //     if (!classEntity) {
+  //       throw new HttpException(`Class with ID ${id} not found`, HttpStatus.NOT_FOUND);
+  //     }
 
-      await this.classRepository.update(id, updateData);
+  //     await this.classRepository.update(id, updateData);
 
-      return {
-        statusCode: HttpStatus.OK,
-        message: 'Class updated successfully',
-      };
-    } catch (error) {
-      throw new HttpException('Internal server error', HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-  }
+  //     return {
+  //       statusCode: HttpStatus.OK,
+  //       message: 'Class updated successfully',
+  //     };
+  //   } catch (error) {
+  //     throw new HttpException('Internal server error', HttpStatus.INTERNAL_SERVER_ERROR);
+  //   }
+  // }
 
-  async updateQuiz(classId: number, dto: UpdateQuizListRequestDto): Promise<ResponseDto> {
-    try {
-      const quizEntity = await this.classRepository.findById(classId);
-      if (!quizEntity) {
-        throw new HttpException(`Quiz with ID ${classId} not found`, HttpStatus.NOT_FOUND);
-      }
+  // async updateQuiz(classId: number, dto: UpdateQuizListRequestDto): Promise<ResponseDto> {
+  //   try {
+  //     const quizEntity = await this.classRepository.findById(classId);
+  //     if (!quizEntity) {
+  //       throw new HttpException(`Quiz with ID ${classId} not found`, HttpStatus.NOT_FOUND);
+  //     }
 
-      await this.quizRepository.updateQuizzes(classId, dto.quizzes);
+  //     await this.quizRepository.updateQuizzes(classId, dto.quizzes);
 
-      return {
-        statusCode: HttpStatus.OK,
-        message: 'Quiz updated successfully',
-      };
-    } catch (error) {
-      throw new HttpException(`error: ${error}`, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-  }
+  //     return {
+  //       statusCode: HttpStatus.OK,
+  //       message: 'Quiz updated successfully',
+  //     };
+  //   } catch (error) {
+  //     throw new HttpException(`error: ${error}`, HttpStatus.INTERNAL_SERVER_ERROR);
+  //   }
+  // }
 }
