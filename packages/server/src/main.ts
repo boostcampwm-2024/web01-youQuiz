2d9ae5b26de5fe229463ae7331f5072e296db706
glaxyt <glaxyt@naver.com>
2024-11-29 21:04:59 +0900

    feat: 전역 validation pipe 적용

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -3,9 +3,19 @@ dotenv.config();
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   app.enableCors();
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
