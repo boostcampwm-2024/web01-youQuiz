bd8d6d0ff61f288e5ce285bb987b51b8fa0c05c5
dooohun <ehgns0305@naver.com>
2024-11-30 18:33:29 +0900

    feat: api response 변경으로 인한 구조 변경

--- packages/client/src/pages/quiz-list/ui/ClassItem.tsx
+++ packages/client/src/pages/quiz-list/ui/ClassItem.tsx
@@ -10,37 +10,31 @@ import DownArrowIcon from '@/shared/assets/icons/down-arrow.svg?react';
 import BinIcon from '@/shared/assets/icons/bin.svg?react';
 import { useDeleteClass } from '@/shared/hooks/classes';
 
-type QuizList = QuizData[];
-
 interface Quiz {
   id: number;
   title: string;
+  quizzes: QuizData[];
 }
 
 interface ClassItemProps {
-  quiz: Quiz;
+  quizList: Quiz;
   index: number;
-  quizList: QuizList[];
 }
 
-export default function ClassItem({ quiz, index, quizList }: ClassItemProps) {
+export default function ClassItem({ index, quizList }: ClassItemProps) {
   const [selectedClassIndex, setSelectedClassIndex] = useState(-1);
 
   const mutation = useDeleteClass();
   const navigate = useNavigate();
 
   const handleSelectClass = (index: number) => {
-    if (selectedClassIndex === index) {
-      setSelectedClassIndex(-1);
-      return;
-    }
-    setSelectedClassIndex(index);
+    setSelectedClassIndex((prevIndex) => (prevIndex === index ? -1 : index));
   };
 
   const handleQuizStart = async (id: number) => {
     const socket = getQuizSocket();
     socket.emit('master entry', { classId: id });
-    /**비동기 작업 */
+
     const sid = await waitForSocketEvent('session', socket);
     setCookie('sid', sid);
 
@@ -50,27 +44,29 @@ export default function ClassItem({ quiz, index, quizList }: ClassItemProps) {
     navigate(`/quiz/wait/${pinCode}`);
   };
 
-  const handleDeleteClass = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleDeleteClass = (e: React.MouseEvent<HTMLButtonElement>, id: number) => {
     e.stopPropagation();
-    mutation.mutate(quiz.id);
+    mutation.mutate(id);
   };
 
   return (
     <>
       <div
-        className={`flex justify-between itemds-center min-w-content h-20 items-center bg-white ${selectedClassIndex === index ? 'border-secondary' : 'border-border'}  border rounded-base p-6 cursor-pointer`}
+        className={`flex justify-between items-center min-w-content h-20 bg-white ${
+          selectedClassIndex === index ? 'border-secondary' : 'border-border'
+        } border rounded-base p-6 cursor-pointer`}
         onClick={() => handleSelectClass(index)}
       >
-        <span>{quiz.title}</span>
+        <span>{quizList.title}</span>
         <div className="flex gap-4">
-          <button type="button" onClick={handleDeleteClass}>
+          <button type="button" onClick={(e) => handleDeleteClass(e, quizList.id)}>
             <BinIcon className="w-5 h-5 text-red-500" />
           </button>
           <CustomButton
             type="full"
             label="퀴즈 시작하기"
             color="secondary"
-            onClick={() => handleQuizStart(quiz.id)}
+            onClick={() => handleQuizStart(quizList.id)}
           />
           <button type="button" onClick={() => handleSelectClass(index)}>
             <DownArrowIcon
@@ -82,11 +78,14 @@ export default function ClassItem({ quiz, index, quizList }: ClassItemProps) {
       </div>
       {selectedClassIndex === index && (
         <div
-          className={`flex flex-col gap-3 p-6 mt-4 border ${selectedClassIndex === index ? 'border-secondary' : 'border-border'} rounded-base bg-white`}
+          className={`flex flex-col gap-3 p-6 -mt-8 border ${
+            selectedClassIndex === index ? 'border-secondary' : 'border-border'
+          } rounded-base bg-white`}
         >
-          {quizList[index].map((quizData, index) => (
-            <span key={quizData.content}>
-              {index + 1}번 문제: {quizData.content}
+          {quizList.quizzes.length === 0 && <span>퀴즈가 없습니다.</span>}
+          {quizList.quizzes.map((quizData, quizIndex) => (
+            <span key={`${quizData.content} ${quizIndex}`}>
+              {quizIndex + 1}번 문제: {quizData.content}
             </span>
           ))}
         </div>
