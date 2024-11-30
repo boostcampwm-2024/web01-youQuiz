49ea716bf3029ee19b1c2529f6318c9ee7f7a74b
byeong <qudcks4885@gmail.com>
2024-11-30 18:16:28 +0900

    feat: Guest, Host Prevent Router 추가
    - sid, pincode 확인 후 네비게이트 허용

--- packages/client/src/app/routes/Router.tsx
+++ packages/client/src/app/routes/Router.tsx
@@ -14,6 +14,8 @@ import GuestQnA from '@/pages/guest-qna';
 import QuizList from '@/pages/quiz-list';
 import QuizMasterSession from '@/pages/quiz-master-session';
 import Leaderboard from '@/pages/leaderboard';
+import PreventGuestRouter from './PreventGuestRouter';
+import PreventHostRouter from './PreventHostRouter';
 
 export default function Router() {
   return (
@@ -25,13 +27,17 @@ export default function Router() {
         <Route path="/questions" element={<QnA />} />
       </Route>
       <Route element={<GuestLayout />}>
-        <Route path="/quiz/session/:pinCode/:id" element={<QuizSession />} />
-        <Route path="/quiz/wait/:pinCode" element={<QuizWait />} />
+        <Route element={<PreventGuestRouter />}>
+          <Route path="/quiz/session/:pinCode/:id" element={<QuizSession />} />
+          <Route path="/quiz/wait/:pinCode" element={<QuizWait />} />
+          <Route path="/guest/questions" element={<GuestQnA />} />
+        </Route>
         <Route path="/nickname/:pinCode" element={<Nickname />} />
-        <Route path="/guest/questions" element={<GuestQnA />} />
       </Route>
       <Route path="/quiz/question" element={<QuizQuestion />} />
-      <Route path="/quiz/session/host/:pinCode/:id" element={<QuizMasterSession />} />
+      <Route element={<PreventHostRouter />}>
+        <Route path="/quiz/session/host/:pinCode/:id" element={<QuizMasterSession />} />
+      </Route>
       <Route path="/quiz/session/end" element={<Leaderboard />} />
       <Route path={'*'} element={<NotFound />} />
     </Routes>
