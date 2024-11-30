f0399fbe44be5ec0644ba120725315e4444a66be
byeong <qudcks4885@gmail.com>
2024-11-30 17:54:32 +0900

    fix: quiz-list에서 뒤로가기 클릭 시 메인 화면으로

--- packages/client/src/shared/ui/buttons/BackButton.tsx
+++ packages/client/src/shared/ui/buttons/BackButton.tsx
@@ -1,10 +1,20 @@
+import { useNavigate } from 'react-router-dom';
 import LeftArrow from '@/shared/assets/icons/left-arrow.svg?react';
 
 export default function BackButton() {
+  const navigate = useNavigate();
+
+  const handleBackBtn = () => {
+    if (window.location.pathname === '/quiz-list') {
+      navigate('/');
+    } else {
+      history.back();
+    }
+  };
   return (
     <button
       className="w-10 h-10 border rounded-full border-none bg-weak flex items-center justify-center"
-      onClick={() => history.back()}
+      onClick={handleBackBtn}
     >
       <LeftArrow className="w-6 h-6" />
     </button>
