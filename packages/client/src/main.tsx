71d4796550494e9d8d619483f360a3bdb030a02c
byeong <qudcks4885@gmail.com>
2024-12-01 17:57:00 +0900

    feat: react-query 기본값 설정 및 devtools 설정

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -5,14 +5,24 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from '@/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 10,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
+        <ReactQueryDevtools initialIsOpen={true} />
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
