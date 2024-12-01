6ff9d044af55a79e44bd71f133b763bd0a34dd16
dooohun <ehgns0305@naver.com>
2024-12-01 18:21:39 +0900

    feat: tickcount를 약수에 맞게 설정 및 에러 수정

--- packages/client/src/pages/quiz-master-session/ui/AnswerChart.tsx
+++ packages/client/src/pages/quiz-master-session/ui/AnswerChart.tsx
@@ -16,35 +16,40 @@ import LoadingSpinner from '@/shared/assets/icons/loading-alt-loop.svg?react';
 
 interface AnswerStatProps {
   answerStats: MasterStatisticsResponse['choiceStatus'];
-  quizData: QuizData;
+  quizData: QuizData | null;
   participantCount: number;
 }
 
+const calculateTickCount = (maxValue: number): number => {
+  const maxTicks = 5;
+
+  const divisors = Array.from({ length: maxValue }, (_, i) => i + 1).filter(
+    (num) => maxValue % num === 0,
+  );
+  return Math.max(divisors[Math.min(divisors.length - 1, maxTicks - 1)], 2);
+};
+
 export default function AnswerGraph({ answerStats, quizData, participantCount }: AnswerStatProps) {
-  const [loadedQuizData, setLoadedQuizData] = useState<QuizData | null>(null);
   const [answerStatsArray, setAnswerStatsArray] = useState<{ answer: string; count: number }[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (quizData) {
-      setLoadedQuizData(quizData);
-
-      const updatedAnswerStatsArray = Object.entries(answerStats).map(([answer, count]) => {
-        const choice = quizData.choices[parseInt(answer)];
-        return {
-          answer: choice
-            ? `${parseInt(answer) + 1}번 ${choice.content}`
-            : `${parseInt(answer) + 1}번 미정`,
-          count,
-        };
-      });
-      setAnswerStatsArray(updatedAnswerStatsArray);
+    if (quizData?.choices) {
+      const formattedData = Object.entries(answerStats).map(([key, count]) => ({
+        answer: `1번: ${quizData.choices[parseInt(key)].content}` || `답변 ${key}`,
+        count,
+      }));
+      setAnswerStatsArray(formattedData);
+      setLoading(false);
     }
   }, [quizData, answerStats]);
 
-  if (!loadedQuizData) {
+  const tickCount = calculateTickCount(participantCount);
+
+  if (loading) {
     return (
-      <div className="flex items-center justify-center w-screen h-screen">
-        <LoadingSpinner className="animate-spin w-16 h-16 md:w-24 md:h-24 lg:w-32 lg:h-32" />
+      <div className="flex items-center justify-center h-full">
+        <LoadingSpinner width={50} height={50} />
       </div>
     );
   }
@@ -54,7 +59,12 @@ export default function AnswerGraph({ answerStats, quizData, participantCount }:
       <BarChart data={answerStatsArray} barSize={60}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="answer" axisLine={false} tickLine={false} />
-        <YAxis axisLine={false} tickLine={false} tickCount={5} domain={[0, participantCount]} />
+        <YAxis
+          axisLine={false}
+          tickLine={false}
+          tickCount={tickCount}
+          domain={[0, participantCount]}
+        />
         <Tooltip formatter={(value: number) => [`${value}명`, '참여자 수']} />
         <Legend formatter={() => '참여자 수'} />
         <Bar dataKey="count" fillOpacity={0.8} />
