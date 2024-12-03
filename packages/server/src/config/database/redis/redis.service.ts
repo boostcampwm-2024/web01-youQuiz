075ab5f6730610424d9477369c893550bdd216a9
nowChae <nowchae23@gmail.com>
2024-12-04 03:33:10 +0900

    feat: redisService 예외 처리를 위한 try catch문 추가

--- packages/server/src/config/database/redis/redis.service.ts
+++ packages/server/src/config/database/redis/redis.service.ts
@@ -1,29 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import Redis from 'ioredis';
 import { InjectRedis } from '@nestjs-modules/ioredis';
+import { RedisException } from '../../../module/exceptions/redis.exception';
 
 @Injectable()
 export class RedisService {
   constructor(@InjectRedis() private readonly redis: Redis) {}
 
   async set(key: string, value: string, ex?: 'EX', expireInSeconds?: number) {
-    if (expireInSeconds) {
-      await this.redis.set(key, value, ex, expireInSeconds);
-    } else {
-      await this.redis.set(key, value);
+    try {
+      if (expireInSeconds) {
+        await this.redis.set(key, value, ex, expireInSeconds);
+      } else {
+        await this.redis.set(key, value);
+      }
+    } catch (error) {
+      throw new RedisException(`Error: Set redis key: ${key}`);
     }
   }
 
   async exists(key: string) {
-    return await this.redis.exists(key);
+    try {
+      return await this.redis.exists(key);
+    } catch (error) {
+      throw new RedisException(`Error: Exists redis key: ${key}`);
+    }
   }
 
   async get(key: string) {
-    return await this.redis.get(key);
+    try {
+      const result = await this.redis.get(key);
+      if (!result) {
+        throw new RedisException(`Key not found: ${key}`, 404);
+      }
+      return result;
+    } catch (error) {
+      throw new RedisException(`Error retrieving Redis key: ${key}`, 500);
+    }
   }
 
   async del(key: string) {
-    await this.redis.del(key);
+    try {
+      await this.redis.del(key);
+    } catch (error) {
+      throw new RedisException(`Error: Del redis key: ${key}`);
+    }
   }
 
   async zincrby(key: string, increment: number, member: string) {
