0c1f6648fce206a7b1e40b597b7e852e666f12b4
glaxyt <glaxyt@naver.com>
2024-12-04 03:17:30 +0900

    refactor: 선택지 엔티티 명시적 매핑 추가

--- packages/server/src/module/quiz/quizzes/entities/choice.entity.ts
+++ packages/server/src/module/quiz/quizzes/entities/choice.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Quiz } from './quiz.entity';
 
-@Entity()
+@Entity('choice')
 export class Choice {
   @PrimaryGeneratedColumn()
   id: number;
@@ -18,10 +18,10 @@ export class Choice {
   @Column()
   position: number;
 
-  @Column({ name: 'created_at' }) 
+  @Column({ name: 'created_at' })
   createdAt: Date;
 
-  @ManyToOne(() => Quiz, quiz => quiz.choices)
-  @JoinColumn({ name: 'quiz_id' })
+  @ManyToOne(() => Quiz, (quiz) => quiz.choices)
+  @JoinColumn({ name: 'quiz_id', referencedColumnName: 'id' })
   quiz: Quiz;
 }
