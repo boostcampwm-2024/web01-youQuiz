9fb9835596ebcd00c4fb74d691bd5b83a830e5dc
dooohun <ehgns0305@naver.com>
2024-12-04 20:50:25 +0900

    fix: 에러 바운더리에 잘못 입력한 값 삭제

--- packages/client/src/shared/boundary/AsyncBoundary.tsx
+++ packages/client/src/shared/boundary/AsyncBoundary.tsx
@@ -5,7 +5,7 @@ import { ErrorBoundary } from 'react-error-boundary';
 export default function AsyncBoundary({ children }: { children: React.ReactNode }) {
   return (
     <ErrorBoundary fallback={<div>안녕하세요?</div>}>
-      <Suspense fallback={<QuizLoading />}>{children}</Suspense>;
+      <Suspense fallback={<QuizLoading />}>{children}</Suspense>
     </ErrorBoundary>
   );
 }
