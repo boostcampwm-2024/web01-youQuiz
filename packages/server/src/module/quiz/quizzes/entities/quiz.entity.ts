0b0310c914401f26431ab277513e827975e8cc3a
glaxyt <glaxyt@naver.com>
2024-12-13 05:00:32 +0900

    refactor: quiz 테이블 명시

--- packages/server/src/module/quiz/quizzes/entities/quiz.entity.ts
+++ packages/server/src/module/quiz/quizzes/entities/quiz.entity.ts
@@ -3,7 +3,7 @@ import { QuizType } from '../utils/quiz-type.enum';
 import { Choice } from './choice.entity';
 import { Class } from './class.entity';
 
-@Entity()
+@Entity('quiz')
 export class Quiz {
   @PrimaryGeneratedColumn()
   id: number;
@@ -15,10 +15,10 @@ export class Quiz {
   content: string;
 
   @Column({
-      name: 'quiz_type',
-      type: 'enum',
-      enum: QuizType,
-      default: QuizType.TF,
+    name: 'quiz_type',
+    type: 'enum',
+    enum: QuizType,
+    default: QuizType.TF,
   })
   quizType: QuizType;
 
@@ -29,18 +29,18 @@ export class Quiz {
   point: number;
 
   @Column()
-  position: number
+  position: number;
 
   @Column({ name: 'created_at' })
   createdAt: Date;
 
-  @OneToMany(() => Choice, choice => choice.quiz, {
+  @OneToMany(() => Choice, (choice) => choice.quiz, {
     cascade: true,
-    eager: false
+    eager: false,
   })
   choices: Choice[];
 
-  @ManyToOne(() => Class, cls => cls.quizzes)
-  @JoinColumn({ name: 'class_id' })
+  @ManyToOne(() => Class, (cls) => cls.quizzes)
+  @JoinColumn([{ name: 'class_id', referencedColumnName: 'id' }])
   class: Class;
 }
